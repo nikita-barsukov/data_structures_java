374e20ce501701c9504c61057cd564b0e22967d9
Nikita Barsukov <nikita@barsukov.net>
2016-04-18 21:02:51 +0200

    Implemented bubble sort argorithm with better tests.

--- src/bubble_sort/BubbleSortTest.java
+++ src/bubble_sort/BubbleSortTest.java
@@ -3,6 +3,9 @@ package bubble_sort;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class BubbleSortTest {
@@ -31,6 +34,25 @@ public class BubbleSortTest {
         int[] arr = new int[] {3,4,1,2,5};
         int[] arr_sorted = bsrt.bubbleSort(arr);
         assertArrayEquals(new int[]{1,2,3,4,5}, arr_sorted);
+    }
+
+    @Test
+    public void bubbleSortRandomArray() {
+        int[] arr = new int[10];
+        Random rnd = new Random();
+        for(int i = 0; i < 10; i++){
+            arr[i] = rnd.nextInt(100);
+        }
+
+        int[] arr_sorted =  bsrt.bubbleSort(arr);
+        System.out.println(Arrays.toString(arr_sorted));
+
+        boolean monotonous_increase = true;
+        for(int i=0; i < arr_sorted.length - 2; i++) {
+            monotonous_increase = !(arr_sorted[i] > arr_sorted[i+1]);
+        }
+        assertTrue(monotonous_increase);
+
     }
     @Before
     public void setUp() throws Exception {
